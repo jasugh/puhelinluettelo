0d9cc37862f1c0d456272a3a25f77a9c2426698f
Jukka <joku@jossain.com>
2019-12-07 00:00:38 +0200

    uibuild

---
+++ build/precache-manifest.79976e59541fb742f0a5da8dcefc1072.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e921975b338e170b8ebcb0495df9f14b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35e1afaf73de2b27ae79",
+    "url": "/static/css/main.4148d5b5.chunk.css"
+  },
+  {
+    "revision": "1c0d0aae31a42ccc5094",
+    "url": "/static/js/2.7a9c70cb.chunk.js"
+  },
+  {
+    "revision": "35e1afaf73de2b27ae79",
+    "url": "/static/js/main.f6b1e48f.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
